ebc00c9e8f65657ff09cea686bd84f56780c36e9
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2026-01-06 21:15:57 +0900

    First version of 'ゆれ'

--- index.js
+++ index.js
@@ -1,15 +1,116 @@
 'use strict';
 
-// WebSocket に接続する
-const ws = new WebSocket('ws://localhost:8080');
+/** ゆれの幅 */
+const yureWidth = 800;
+/** ゆれの高さ */
+const yureHeight = 200;
 
-// サーバからメッセージを受信したら output に表示する
+/** 時間の縮小率 */
+const tDiv = 50;
+/** ゆれの拡大率 */
+const xMul = 100;
+/** X 方向のゆれの位置 */
+const xOff = 50;
+/** Y 方向のゆれの位置 */
+const yOff = 0;
+/** Z 方向のゆれの位置 */
+const zOff = -50;
+
+/** 通信用の WebSocket */
+const ws = new WebSocket('./');
+
+/** 全ゆれ記憶バッファ */
+const yures = {};
+
+/** 強制更新タイマバッファ */
+const timers = {};
+
+/** 表示を更新する */
+async function
+update(yureId)
+{
+	const cv = new OffscreenCanvas(yureWidth, yureHeight);
+	const ctx = cv.getContext('2d');
+	ctx.setTransform(-1, 0, 0, -1, yureWidth, yureHeight / 2);
+
+	// 横線の描画
+	ctx.strokeStyle = 'rgba(0,0,0,.25)';
+	ctx.beginPath();
+	ctx.moveTo(0, xOff);
+	ctx.lineTo(yureWidth, xOff);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(0, yOff);
+	ctx.lineTo(yureWidth, yOff);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(0, zOff);
+	ctx.lineTo(yureWidth, zOff);
+	ctx.stroke();
+
+	// 縦線の描画
+	for (let i = 0; i < yureWidth; i += 1000 / tDiv) {
+		ctx.beginPath();
+		ctx.moveTo(i, -yureHeight);
+		ctx.lineTo(i, +yureHeight);
+		ctx.stroke();
+	}
+
+	ctx.strokeStyle = 'red';
+	ctx.beginPath();
+	yures[yureId].forEach(e => {
+		ctx.lineTo((Date.now() - e.t) / tDiv, e.x * xMul + xOff);
+	});
+	ctx.stroke();
+
+	ctx.strokeStyle = 'green';
+	ctx.beginPath();
+	yures[yureId].forEach(e => {
+		ctx.lineTo((Date.now() - e.t) / tDiv, e.y * xMul + yOff);
+	});
+	ctx.stroke();
+
+	ctx.strokeStyle = 'blue';
+	ctx.beginPath();
+	yures[yureId].forEach(e => {
+		ctx.lineTo((Date.now() - e.t) / tDiv, e.z * xMul + zOff);
+	});
+	ctx.stroke();
+
+	const blob = await cv.convertToBlob();
+	const url = URL.createObjectURL(blob);
+	document.getElementById(`img${yureId}`)?.setAttribute('src', url);
+	URL.revokeObjectURL(url);
+}
+
+function
+forcedUpdate()
+{
+	Object.keys(yures).forEach(yureId => update(yureId));
+	requestAnimationFrame(forcedUpdate);
+}
+
 ws.addEventListener('message', e => {
-	output.textContent += e.data + '\n';
-});
+	const buf = JSON.parse(e.data);
 
-// フォームを送信しようとしたら WebSocket でサーバにメッセージを送る
-form.addEventListener('submit', e => {
-	e.preventDefault(); // form の送信を行わない
-	ws.send(msg.value);
+	// 知らないゆれ識別子が来たら登録する
+	const yureId = buf[0].yureId;
+	if (!yures[yureId]) {
+		yures[yureId] = [ ];
+
+		// 表示領域の準備
+		const h3 = document.createElement('h3');
+		h3.textContent = yureId;
+		const img = document.createElement('img');
+		img.setAttribute('id', `img${yureId}`);
+		const section = document.createElement('section');
+		section.setAttribute('id', yureId);
+		section.append(h3, img);
+		main.append(section);
+	}
+
+	// 加速度を記憶する（長過ぎたら切り詰める）
+	yures[yureId].unshift(...buf.toReversed());
+	yures[yureId].length = 3000;
 });
+requestAnimationFrame(forcedUpdate);
---
+++ sensor.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** ゆれ識別子（33 bit）を生成する */
+function
+generateYureId()
+{
+	const r8 = _ => Math.random() * 8 | 0;
+	return [...Array(11)].map(_ => 'YUREyure'[r8()]).join('');
+}
+
+// ゆれ識別子を設定する
+const yureId = localStorage.getItem('yureId') || generateYureId();
+localStorage.setItem('yureId', yureId);
+outYureId.value = yureId;
+
+/** 通信用 WebSocket */
+const ws = new WebSocket('./');
+
+/** 加速度データバッファ */
+const buf = [];
+
+/** センサイベントハンドラ */
+function
+devMotionHandler(e)
+{
+	buf.push({
+		yureId,
+		...e.acceleration,
+		t: Date.now(),
+	});
+	if (buf.length === 30) {
+		ws.send(JSON.stringify(buf));
+		buf.length = 0;
+	}
+}
+
+// フォームを送信しようとしたら WebSocket でサーバにメッセージを送る
+form.addEventListener('submit', async e => {
+	// form の送信を行わない
+	e.preventDefault();
+
+	// ゆれをシェアしようとしている？
+	if (e.submitter === btnStart) {
+		// iOS の Safari は正気かどうかを確かめてくる
+		if (DeviceMotionEvent?.requestPermission) {
+			const p = await DeviceMotionEvent.requestPermission();
+			if (p !== 'granted')
+				return;
+		}
+
+		// センサの監視をはじめる
+		window.addEventListener('devicemotion', devMotionHandler);
+		btnStart.disabled = true;
+		btnStop.disabled = false;
+	}
+
+	// ゆれをシェアしまいとしている？
+	if (e.submitter === btnStop) {
+		window.removeEventListener('devicemotion', devMotionHandler);
+		btnStart.disabled = false
+		btnStop.disabled = true;
+	}
+});
